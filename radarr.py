bc3c02c91d12ec7bae33a392b4dbade56a743c03
toddrob <toddrob99@users.noreply.github.com>
2020-07-03 14:40:15 -0400

    Coding Standards, add .dockerignore, fix typo, language tweaks (#21)
    * format code with black, add .dockerignore, update dockerfile to alpine3.8, add flake8 config, address problems identified by flake8, fix sickrage typo, refactor code in a couple areas, update en lang 'serie' to 'series'
    
    * Add admin.txt and container name to docker-compose
    
    * Code formatting and fixed linting warnings
    
    * Code formatting

--- radarr.py
+++ radarr.py
@@ -1,26 +1,30 @@
 #!/usr/bin/env python3
 
-import requests, json, sys, logging
 import commons as commons
+import json
+import logging
+import requests
 import yaml
 
-from definitions import CONFIG_PATH, LANG_PATH, LOG_PATH
+from definitions import CONFIG_PATH, LOG_PATH
 
 log = logging
-log.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', filename=LOG_PATH,
-                        filemode='a',
-                        level=logging.INFO)
+log.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    filename=LOG_PATH,
+    filemode="a",
+    level=logging.INFO,
+)
 
-config = yaml.safe_load(open(CONFIG_PATH, encoding='utf8'))
+config = yaml.safe_load(open(CONFIG_PATH, encoding="utf8"))
 config = config["radarr"]
 
-addMovieNeededFields = ['tmdbId', 'year', 'title', 'titleSlug', 'images']
+addMovieNeededFields = ["tmdbId", "year", "title", "titleSlug", "images"]
 
-def search(title): 
-    parameters = {
-        "term": title
-    }
-    req = requests.get(commons.generateApiQuery('radarr', 'movie/lookup', parameters))
+
+def search(title):
+    parameters = {"term": title}
+    req = requests.get(commons.generateApiQuery("radarr", "movie/lookup", parameters))
     parsed_json = json.loads(req.text)
 
     if req.status_code == 200:
@@ -28,62 +32,68 @@ def search(title):
     else:
         return False
 
+
 def giveTitles(parsed_json):
     data = []
     for movie in parsed_json:
-        if all(x in movie for x in ['title', 'overview', 'remotePoster', 'year', 'tmdbId']):
-            data.append({
-                'title': movie['title'],
-                'overview': movie['overview'],
-                'poster': movie['remotePoster'],
-                'year': movie['year'],
-                'id': movie['tmdbId']
-            })
+        if all(
+            x in movie for x in ["title", "overview", "remotePoster", "year", "tmdbId"]
+        ):
+            data.append(
+                {
+                    "title": movie["title"],
+                    "overview": movie["overview"],
+                    "poster": movie["remotePoster"],
+                    "year": movie["year"],
+                    "id": movie["tmdbId"],
+                }
+            )
     return data
+
 
 def returnVal(rawjson):
     totalData = []
     moviesList = rawjson
     for movie in moviesList:
         specMovie = moviesList[movie]
-        totalData.append(specMovie["title"], specMovie["tmdbId"], specMovie["remotePoster"], specMovie["overview"], specMovie["year"]) 
+        totalData.append(
+            specMovie["title"],
+            specMovie["tmdbId"],
+            specMovie["remotePoster"],
+            specMovie["overview"],
+            specMovie["year"],
+        )
     return totalData
+
 
 def inLibrary(tmdbId):
     parameters = {}
-    req = requests.get(commons.generateApiQuery('radarr', 'movie', parameters))
+    req = requests.get(commons.generateApiQuery("radarr", "movie", parameters))
     parsed_json = json.loads(req.text)
-    for movie in parsed_json:
-        if movie['tmdbId'] == tmdbId:
-            return True
-            break
-    return False
+    return next((True for movie in parsed_json if movie["tmdbId"] == tmdbId), False)
+
 
 def addToLibrary(tmdbId):
-    parameters = {
-        "tmdbId": str(tmdbId)
-    }
-    req = requests.get(commons.generateApiQuery('radarr', 'movie/lookup/tmdb', parameters))
+    parameters = {"tmdbId": str(tmdbId)}
+    req = requests.get(
+        commons.generateApiQuery("radarr", "movie/lookup/tmdb", parameters)
+    )
     parsed_json = json.loads(req.text)
     data = json.dumps(buildData(parsed_json))
-    add = requests.post(commons.generateApiQuery('radarr', 'movie'), data=data)
+    add = requests.post(commons.generateApiQuery("radarr", "movie"), data=data)
     if add.status_code == 201:
         return True
     else:
         return False
 
+
 def buildData(json):
     built_data = {
-        'qualityProfileId': config["qualityProfileId"],
-        'rootFolderPath': config["rootFolder"],
-        "addOptions" : {
-            "searchForMovie" : config["search"]},
-        }
+        "qualityProfileId": config["qualityProfileId"],
+        "rootFolderPath": config["rootFolder"],
+        "addOptions": {"searchForMovie": config["search"]},
+    }
 
     for key in addMovieNeededFields:
         built_data[key] = json[key]
     return built_data
-
-
-
-
--- sonarr.py
+++ sonarr.py
@@ -1,27 +1,30 @@
 #!/usr/bin/env python3
 
-import requests, json, sys, logging
 import commons as commons
+import json
+import logging
+import requests
 import yaml
 
-from definitions import CONFIG_PATH, LANG_PATH, LOG_PATH
+from definitions import CONFIG_PATH, LOG_PATH
 
 log = logging
-log.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', filename=LOG_PATH,
-                        filemode='a',
-                        level=logging.INFO)
+log.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    filename=LOG_PATH,
+    filemode="a",
+    level=logging.INFO,
+)
 
-config = yaml.safe_load(open(CONFIG_PATH, encoding='utf8'))
+config = yaml.safe_load(open(CONFIG_PATH, encoding="utf8"))
 config = config["sonarr"]
 
-addSerieNeededFields = ['tvdbId', 'tvTageId', 'title', 'titleSlug', 'images', 'seasons']
-                          
+addSerieNeededFields = ["tvdbId", "tvRageId", "title", "titleSlug", "images", "seasons"]
 
-def search(title): 
-    parameters = {
-        "term": title
-    }
-    req = requests.get(commons.generateApiQuery('sonarr', 'series/lookup', parameters))
+
+def search(title):
+    parameters = {"term": title}
+    req = requests.get(commons.generateApiQuery("sonarr", "series/lookup", parameters))
     parsed_json = json.loads(req.text)
 
     if req.status_code == 200:
@@ -29,66 +32,74 @@ def search(title):
     else:
         return False
 
+
 def giveTitles(parsed_json):
     data = []
     for show in parsed_json:
-        if all(x in show for x in ['title', 'seasonCount', 'remotePoster', 'year', 'tvdbId']):
-            data.append({
-                'title': show['title'],
-                'seasonCount': show['seasonCount'],
-                'poster': show['remotePoster'],
-                'year': show['year'],
-                'id': show['tvdbId']
-            })
+        if all(
+            x in show
+            for x in ["title", "seasonCount", "remotePoster", "year", "tvdbId"]
+        ):
+            data.append(
+                {
+                    "title": show["title"],
+                    "seasonCount": show["seasonCount"],
+                    "poster": show["remotePoster"],
+                    "year": show["year"],
+                    "id": show["tvdbId"],
+                }
+            )
     return data
+
 
 def returnVal(rawjson):
     totalData = []
     seriesList = rawjson
     for show in seriesList:
         specShow = seriesList[show]
-        totalData.append(specShow["title"], specShow["itvDb"], specShow["remotePoster"], specShow["seasonCount"], specShow["year"]) 
+        totalData.append(
+            specShow["title"],
+            specShow["itvDb"],
+            specShow["remotePoster"],
+            specShow["seasonCount"],
+            specShow["year"],
+        )
     return totalData
+
 
 def inLibrary(tvdbId):
     parameters = {}
-    req = requests.get(commons.generateApiQuery('sonarr', 'series', parameters))
+    req = requests.get(commons.generateApiQuery("sonarr", "series", parameters))
     parsed_json = json.loads(req.text)
-    for show in parsed_json:
-        if show['tvdbId'] == tvdbId:
-            return True
-            break
-    return False
+    return next((True for show in parsed_json if show["tvdbId"] == tvdbId), False)
+
 
 def addToLibrary(tvdbId):
-    parameters = {
-        "term": "tvdb:" + str(tvdbId)
-    }
-    req = requests.get(commons.generateApiQuery('sonarr', 'series/lookup', parameters))
+    parameters = {"term": "tvdb:" + str(tvdbId)}
+    req = requests.get(commons.generateApiQuery("sonarr", "series/lookup", parameters))
     parsed_json = json.loads(req.text)
     data = json.dumps(buildData(parsed_json))
-    add = requests.post(commons.generateApiQuery('sonarr', 'series'), data=data)
+    add = requests.post(commons.generateApiQuery("sonarr", "series"), data=data)
     if add.status_code == 201:
         return True
     else:
         return False
 
+
 def buildData(json):
     built_data = {
-        'qualityProfileId': config["qualityProfileId"],
-        'addOptions': {'ignoreEpisodesWithFiles': 'true',
-                        'ignoreEpisodesWithoutFiles': 'false',
-                        'searchForMissingEpisodes': config["search"]},
-        'rootFolderPath': config["rootFolder"],
-        'seasonFolder': config["seasonFolder"]                          
-        }
+        "qualityProfileId": config["qualityProfileId"],
+        "addOptions": {
+            "ignoreEpisodesWithFiles": "true",
+            "ignoreEpisodesWithoutFiles": "false",
+            "searchForMissingEpisodes": config["search"],
+        },
+        "rootFolderPath": config["rootFolder"],
+        "seasonFolder": config["seasonFolder"],
+    }
 
     for show in json:
         for key, value in show.items():
             if key in addSerieNeededFields:
                 built_data[key] = value
     return built_data
-
-
-
-
