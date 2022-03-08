65ab95f82385b72a6eb7d528b86926009a5ad1ac
Wannes Van de Putte <vandeputte.wannes@gmail.com>
2022-03-08 22:37:17 +0100

    Change ROOT_DIR

--- src/definitions.py
+++ src/definitions.py
@@ -1,7 +1,9 @@
 import os
+from pathlib import Path
+
 
 # Set Projects Root Directory
-ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+ROOT_DIR = os.path.dirname(os.path.dirname(os.path.normpath(__file__)))
 
 # Set Projects Configuration path
 CONFIG_PATH = os.path.join(ROOT_DIR, "config.yaml")
