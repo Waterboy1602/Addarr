a84f7f6971d8f100c41633c1716612f83f9af90f
anast20sm <asm9720@gmail.com>
2023-10-11 13:27:35 +0200

    Fix removal mistakes

--- src/config.py
+++ src/config.py
@@ -5,15 +5,18 @@ from definitions import CONFIG_PATH, CONFIG_EXAMPLE_PATH, DEFAULT_SETTINGS
 config = yaml.safe_load(open(CONFIG_PATH, encoding="utf8"))
 config_example = yaml.safe_load(open(CONFIG_EXAMPLE_PATH, encoding="utf8"))
 
+
 for setting, default_value in DEFAULT_SETTINGS.items():
     if setting not in config:
         config[setting] = default_value
+
 
 def flatten_dict(dd, separator ='/', prefix =''):
     return { prefix + separator + k if prefix else k : v
              for kk, vv in dd.items()
              for k, v in flatten_dict(vv, separator, kk).items()
              } if isinstance(dd, dict) else { prefix : dd }
+
 
 def checkConfig():
     missingConfig=[]
@@ -22,9 +25,10 @@ def checkConfig():
             missingConfig.append(key_ex)
     return missingConfig
 
+
 def checkConfigValues():
     wrongValues = []
-    languages = ["en-us", "es-es", "it-it", "nl-be", "pt-pt", "pl-pl", "de-de"]
+    languages = ["en-us", "es-es", "it-it", "nl-be", "pt-pt", "pl-pl", "de-de", "fr-fr", "ru-ru"]
     if config["language"] not in languages:
         wrongValues.append("language")
     return wrongValues
